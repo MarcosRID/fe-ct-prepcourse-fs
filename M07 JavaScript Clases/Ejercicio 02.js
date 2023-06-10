606e65309bfd14eefaa8e263908fa18d039f982b
MRID54543@hotmail.com <MRID54543@hotmail.com>
2023-06-10 19:27:03 -0300

    Parte 1 realizada CLASES

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -6,6 +6,20 @@ class Persona {
    // Debe tener las propiedades: "nombre", "apellido", "edad" y "domicilio".
    // Debe tener un método llamado "detalle" que nos devuelva un objeto con las propiedades de la persona y
    // sus valores.
+
+   constructor(nombre,apellido,edad,domicilio)
+   {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+      this.domicilio = domicilio;
+   }
+
+   detalle()
+   {
+      
+   }
+
 }
 
 function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
